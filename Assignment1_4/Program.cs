41c7f9d088dc64466f38f4bb63acab97951e44d5
unknown <yerassylgumarov@gmail.com>
2023-04-09 13:06:43 +0600

    Initial commit

---
+++ Assignment1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿static ulong  Factorial(ulong n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return n * Factorial(n - 1);
+    
+}
+
+ulong n = ulong.Parse(Console.ReadLine());
+Console.WriteLine(Factorial(n));
